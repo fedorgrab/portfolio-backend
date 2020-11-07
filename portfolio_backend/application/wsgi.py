a66745339630afbd795efc40f350f07b83e7745d
Fedor Grab <fvgrab@gmail.com>
2020-11-07 14:37:13 -0500

    Fix wsgi.py import

--- portfolio_backend/application/wsgi.py
+++ portfolio_backend/application/wsgi.py
@@ -2,9 +2,9 @@
 import os
 import sys
 
+sys.path.append(os.path.realpath(os.path.join(os.path.dirname(__file__), "../", "../")))
 from portfolio_backend.application import backend_application  # noqa
 
-sys.path.append(os.path.realpath(os.path.join(os.path.dirname(__file__), "../", "../")))
 
 if __name__ == "__main__":
     backend_application.run(host="0.0.0.0")
